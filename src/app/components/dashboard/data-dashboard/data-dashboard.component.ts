5ea8117fc872b6af634daeff60af434e30b832dc
Moritz Vogt <moritz.vogt@raysono.com>
2023-09-20 19:37:08 +0200

    Delete Device

--- src/app/components/dashboard/data-dashboard/data-dashboard.component.ts
+++ src/app/components/dashboard/data-dashboard/data-dashboard.component.ts
@@ -1,5 +1,6 @@
 import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
 import * as d3 from 'd3';
+
 @Component({
   selector: 'app-data-dashboard',
   templateUrl: './data-dashboard.component.html',
@@ -8,58 +9,260 @@ import * as d3 from 'd3';
 export class DataDashboardComponent implements AfterViewInit {
   @ViewChild('chart') private chartContainer!: ElementRef;
   private svg: any;
-  private circle: any;
 
-  constructor() {
-
-  }
+  strokeWidth = 3;
+  duration = 3500;
+  strokeFill = '#d8d8d8';
 
   private createChart(): void {
     const element = this.chartContainer.nativeElement;
-    this.svg = d3.select(element).append('svg');
+    this.svg = d3.select(element).append('svg')
+      .attr('width', '70vw') // Breite auf 100% der übergeordneten Komponente setzen
+      .attr('height', '70vh') // Höhe auf 100% der übergeordneten Komponente setzen
+      .attr('preserveAspectRatio', 'xMidYMid meet'); // xMidYMid bewirkt, dass das SVG zentriert bleibt
 
-    // Beispiel für einen Pfeil, der den Stromfluss darstellt
+    // Plant
+    d3.xml('assets/svg/plant.svg').then(data => {
+      const importedNode = document.importNode(data.documentElement, true);
+      const importedSVG = d3.select(importedNode);
+
+      // Positioniere das importierte SVG-Element an den gewünschten Koordinaten (z.B., x=100, y=100)
+      importedSVG
+        .attr('x', 570)
+        .attr('y', 290)
+        .attr('width', 150)
+        .attr('height', 150);
+
+      this.svg.node().appendChild(importedNode);
+    })
+
+    // Pumpe
+    d3.xml('assets/svg/pump.svg').then(data => {
+      const importedNode = document.importNode(data.documentElement, true);
+      const importedSVG = d3.select(importedNode);
+
+      importedSVG
+        .attr('x', 1010)
+        .attr('y', 100)
+        .attr('width', 70)
+        .attr('height', 70);
+
+      this.svg.node().appendChild(importedNode);
+      this.svg.append('text')
+        .attr('x', 1020)
+        .attr('y', 205)
+        .text('Wärmepumpe')
+        .style('font-size', '15px')
+        .style('fill', '#9c9898');
+    })
+
+    // Pumpe
+    d3.xml('assets/svg/home.svg').then(data => {
+      const importedNode = document.importNode(data.documentElement, true);
+      const importedSVG = d3.select(importedNode);
+
+      importedSVG
+        .attr('x', 1010)
+        .attr('y', 350)
+        .attr('width', 70)
+        .attr('height', 70);
+
+      this.svg.node().appendChild(importedNode);
+
+      this.svg.append('text')
+        .attr('x', 1020)
+        .attr('y', 455)
+        .text('Haushaltsverbrauch')
+        .style('font-size', '15px')
+        .style('fill', '#9c9898');
+    })
+
+    // Auto
+    d3.xml('assets/svg/pump.svg').then(data => {
+      const importedNode = document.importNode(data.documentElement, true);
+      const importedSVG = d3.select(importedNode);
+
+      importedSVG
+        .attr('x', 1010)
+        .attr('y', 500)
+        .attr('width', 70)
+        .attr('height', 70);
+
+      this.svg.node().appendChild(importedNode);
+
+      this.svg.append('text')
+        .attr('x', 1020)
+        .attr('y', 605)
+        .text('E-Auto')
+        .style('font-size', '15px')
+        .style('fill', '#9c9898');
+    })
+
+    // Batterie
+    d3.xml('assets/svg/battery.svg').then(data => {
+      const importedNode = document.importNode(data.documentElement, true);
+      const importedSVG = d3.select(importedNode);
+
+      importedSVG
+        .attr('x', 500)
+        .attr('y', 580)
+        .attr('width', 70)
+        .attr('height', 70);
+
+      this.svg.node().appendChild(importedNode);
+
+      this.svg.append('text')
+        .attr('x', 505)
+        .attr('y', 660)
+        .text('Batterie')
+        .style('font-size', '15px')
+        .style('fill', '#9c9898');
+    })
+
     this.svg
       .append('line')
-      .attr('x1', 50)
-      .attr('y1', 50)
-      .attr('x2', 200)
-      .attr('y2', 50)
-      .attr('stroke', '#d8d8d8')
-      .attr('stroke-width', 2)
-      .attr('marker-end', 'url(#arrow)');
+      .attr('x1', 630)
+      .attr('y1', 265)
+      .attr('x2', 630)
+      .attr('y2', 150)
+      .attr('stroke', this.strokeFill)
+      .attr('stroke-width', this.strokeWidth)
 
-    this.circle = this.svg
+    this.svg
+      .append('line')
+      .attr('x1', 630)
+      .attr('y1', 450)
+      .attr('x2', 630)
+      .attr('y2', 565)
+      .attr('stroke', this.strokeFill)
+      .attr('stroke-width', this.strokeWidth)
+
+
+    // Haushalt
+    this.svg
+      .append('line')
+      .attr('x1', 750)
+      .attr('y1', 370)
+      .attr('x2', 950)
+      .attr('y2', 370)
+      .attr('stroke', this.strokeFill)
+      .attr('stroke-width', this.strokeWidth)
+
+    // Wärmepumpe
+    this.svg
+      .append('line')
+      .attr('x1', 950)
+      .attr('x2', 950)
+      .attr('y1', 370)
+      .attr('y2', 200)
+      .attr('stroke', this.strokeFill)
+      .attr('stroke-width', this.strokeWidth)
+
+    this.svg
+      .append('line')
+      .attr('x1', 950)
+      .attr('x2', 1000)
+      .attr('y1', 200)
+      .attr('y2', 200)
+      .attr('stroke', this.strokeFill)
+      .attr('stroke-width', this.strokeWidth)
+
+
+    // Haushaltverbrauch & E-Auto
+    this.svg
+      .append('line')
+      .attr('x1', 950)
+      .attr('x2', 950)
+      .attr('y1', 370)
+      .attr('y2', 600)
+      .attr('stroke', this.strokeFill)
+      .attr('stroke-width', this.strokeWidth)
+
+    // Verbrauch
+    this.svg
+      .append('line')
+      .attr('x1', 950)
+      .attr('x2', 1000)
+      .attr('y1', 450)
+      .attr('y2', 450)
+      .attr('stroke', this.strokeFill)
+      .attr('stroke-width', this.strokeWidth)
+
+    // E-Auto
+    this.svg
+      .append('line')
+      .attr('x1', 950)
+      .attr('x2', 1000)
+      .attr('y1', 600)
+      .attr('y2', 600)
+      .attr('stroke', this.strokeFill)
+      .attr('stroke-width', this.strokeWidth)
+
+
+    let circleGrid = this.svg
       .append('circle')
-      .attr('cx', 50)
-      .attr('cy', 50)
-      .attr('r', 5)
+      .attr('cx', 630)
+      .attr('cy', 265)
+      .attr('r', 7)
       .attr('fill', 'yellow');
 
+    let circleBattery = this.svg
+      .append('circle')
+      .attr('cx', 630)
+      .attr('cy', 450)
+      .attr('r', 7)
+      .attr('fill', 'yellow');
+
+    let circleHaushalt = this.svg
+      .append('circle')
+      .attr('cx', 750)
+      .attr('cy', 370)
+      .attr('r', 7)
+      .attr('fill', 'red');
+
+
+
+    this.animateCircleY(circleGrid, 265, 150);
+    this.animateCircleY(circleBattery, 450, 565);
+    this.animateCircleX(circleHaushalt, 750, 950);
+
     // Animation
-    this.animateCircle();
   }
 
-  private animateCircle(): void {
-    const duration = 3500; // Dauer der Hin- und Herbewegung (2 Sekunden)
-    const startX = 50;
-    const endX = 200;
+  private animateCircleY(circle: any, start: any, end: any): void {
 
-    this.circle
+    circle
       .transition()
-      .duration(duration)
-      .attr('cx', endX)
+      .duration(this.duration)
+      .attr('cy', end)
       .on('end', () => {
-        // Nachdem die Animation abgeschlossen ist, den Kreis ausblenden
-        this.circle
+        circle
           .style('opacity', 0)
           .transition()
-          .duration(0) // Keine Dauer, da wir sofort zurückbewegen wollen
-          .attr('cx', startX)
-          .style('opacity', 1) // Kreis wieder einblenden
+          .duration(0)
+          .attr('cy', start)
+          .style('opacity', 1)
           .on('end', () => {
-            // Die Animation erneut starten, um den Zyklus zu wiederholen
-            this.animateCircle();
+            this.animateCircleY(circle, start, end);
+          });
+      });
+  }
+
+  private animateCircleX(circle: any, start: any, end: any): void {
+
+    circle
+      .transition()
+      .duration(this.duration)
+      .attr('cx', end)
+      .on('end', () => {
+        circle
+          .style('opacity', 0)
+          .transition()
+          .duration(0)
+          .attr('cx', start)
+          .style('opacity', 1)
+          .on('end', () => {
+            this.animateCircleX(circle, start, end);
           });
       });
   }
